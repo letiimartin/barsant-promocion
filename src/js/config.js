bada8892228630b5e442928aa134465e723951b3
letimartin <leticia.martin.cabrera@hotmail.es>
2025-05-25 21:13:31 +0200

    Implement secure password hashing

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@
  * Este archivo contiene variables sensibles y debe estar en .gitignore
  */
 
-// Hash de la contraseña para acceso al sitio (Ventanilla_2025)
+// Hash de la contraseña para acceso al sitio
 export const PASSWORD_HASH = '8dc9d0ec05a1f9f5242b04e567159f7651db8dfa1be4ecaaf59073ed5d5e7f91';

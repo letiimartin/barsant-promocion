e0bd05b7dc8e464135d5c98d28f3a54ab6bef7df
letimartin <leticia.martin.cabrera@hotmail.es>
2025-05-26 19:57:33 +0200

    Add botton to hidden and show password in login

--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Archivo de configuración
- * Este archivo contiene variables sensibles y debe estar en .gitignore
- */
-
-// Hash de la contraseña para acceso al sitio
-export const PASSWORD_HASH = '5012893daa2f0413178e3e5d8e42dd4e23a0642eae6e84ecd8713fd4e671f621';
